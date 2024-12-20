55345a6f85b84941be07805b81c80bc8e746d757
eunphillee <ph2lspeed@gmail.com>
2024-12-20 17:43:56 +0900

    To enlarge the box, press + or - ?

--- day4-react/src/component/Box.js
+++ day4-react/src/component/Box.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //위부에서 사용할 컴포넌트 
 function Box() {
-    const boxsize = {
-        width: 100,
-        height: 100,
-    }
+    // 값 저장 일반객체 
+    // const boxsize = {
+    //     width: 100,
+    //     height: 100,
+    // }
      // boxsize 변수를 상태 변수로 정의하세요. 초기값은 boxsize 입니다.
      
-    const[size,setSize] = useState(boxsize)
+    const [width,setWidth] = useState(100)
+    const [height,setheight] = useState(100)
 
     const handleResize = (e) => {
         switch (e.target.id) {
             case "width_inc":
-                boxsize.width+=10
+                setWidth(width+10)
                 break;
             case "width_dec":
-                boxsize.width-=10
+                setWidth(width-10)
                 break;
             case "height_inc":
-                boxsize.height+=10
+                setheight(height+10)
                 break;
             case "height_dec":
-                boxsize.height-=10
+                setheight(height-10)
                 break;
             default:
                 break;
@@ -36,6 +38,7 @@ function Box() {
             <button onClick={handleResize} id ="height_inc">세로+</button>
             <button onClick={handleResize} id ="height_dec">세로-</button>
         <div id='container'>
+            {/* SandBox 렌더링 하기 위해 너비, 높이를 전달해 줍니다.  */}
             <SandBox size={boxsize}/>
         </div>
         </>
@@ -44,6 +47,8 @@ function Box() {
 
 // 이 파일 안에서만 사용할 컴포넌트 
 function SandBox({size}){
+    // 스타일 적용할 객체
+    // Box 컴포넌ㄴ트에서 전해준 너비, 높이를 스타일 객체에 적용합니다. 
     const boxstyle = {
         width: `${size.width}px`,
         height: `${size.height}px`,
