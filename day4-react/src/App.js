94666dff16206b1ea76f099eda073c3cd65bd4d6
eunphillee <ph2lspeed@gmail.com>
2024-12-23 18:04:01 +0900

    6th period completed

--- day4-react/src/App.js
+++ day4-react/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Box from './component/Box';
 import './App.css'
 import BoxV2 from './component/BoxV2';
+import BoxReducer from './component/BoxReducer';
+import Calculate from './component/CalculateReducer';
 
 function App() {
   return (
     <div>
-      <BoxV2/>
+      <Calculate/>
     </div>
   );
 }
